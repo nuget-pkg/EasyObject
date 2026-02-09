e15dfa39087dc911b2fe47a5a7fc5a671bb8a02b
JavaCommons Technologies <javacommons@gmail.com>
2026-02-09 21:53:52 +0900

    .

--- Targets/Program.cs
+++ Targets/Program.cs
@@ -1,9 +1,11 @@
 ﻿using static Bullseye.Targets;
 using static SimpleExec.Command;
 
-Target("build", () => RunAsync("dotnet", "build --configuration Release"));
+Target("sed",  () => RunAsync("bash", "-c '_sed.sh'"))
+Target("build", () => RunAsync("dotnet", "build -p:Configuration=Release"));
 Target("test", dependsOn: ["build"], () => RunAsync("dotnet", "test --configuration Release --no-build"));
-Target("release", dependsOn: ["test"], () => RunAsync("bash", "-c '_release.sh'"));
+Target("pack", dependsOn: ["test"], () => RunAsync("dotnet", "pack -o . -p:Configuration=Release"));
+Target("release", dependsOn: ["pack"], () => RunAsync("bash", "-c '_release.sh'"));
 Target("default", dependsOn: ["test"]);
 
 await RunTargetsAndExitAsync(args, ex => ex is SimpleExec.ExitCodeException);
