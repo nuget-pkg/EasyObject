7f5b80abfff95687166c8cd87706984778293434
javacommons <javacommons@gmail.com>
2026-03-30 03:10:01 +0900

    .

---
+++ EasyObject.Misc/Misc01.cs
@@ -0,0 +1,20 @@
+﻿// ReSharper disable RedundantUsingDirective
+namespace Misc;
+
+using Global;
+using static Global.EasyObject;
+using static Global.EasySystem;
+
+public class Misc01
+{
+    public static void Main(string[] args)
+    {
+        SetupConsoleEncoding();
+        ShowLineNumbers = false;
+        ShowDetail = true;
+        UseAnsiConsole = true;
+        DebugOutput = true;
+        Log("⭕️ハロー©⭕️");
+        EasySystem.RunToConsole("bash", ["-c", "ls -ltr"]);
+    }
+}
