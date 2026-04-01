8173ab87e205c2600cd3acc5cdb7fe18e6db3054
javacommons <javacommons@gmail.com>
2026-04-01 21:30:36 +0900

    .

--- +sub/detect-dll-info/detect-dll-info.main.cs
+++ +sub/detect-dll-info/detect-dll-info.main.cs
@@ -19,8 +19,14 @@ try
     ShowLineNumbers = false;
     Log("ハロー©");
     Log(new { args });
+
     Log(IsDotNetAssembly(@"C:\home17\cmd\busybox64u.exe"));
     Log(IsDotNetAssembly(@"C:\home17\cmd\MyClass1.dll"));
+    Log(IsDotNetAssembly(@"C:\env\+cmd\wingen.exe"));
+    //"C:\env\+cmd\wingen.exe"
+
+    Log(GetDllArchitecture(@"C:\home17\cmd\busybox64u.exe"));
+    Log(GetDllArchitecture(@"C:\home17\cmd\MyClass1.dll"));
     //"C:\home17\cmd\MyClass1.dll"
 }
 catch (Exception e)
@@ -28,10 +34,23 @@ catch (Exception e)
     Abort(e);
 }
 
-/*public static*/ bool IsDotNetAssembly(string fileName)
+bool IsDotNetAssembly(string fileName)
 {
     using var stream = File.OpenRead(fileName);
     using var peReader = new PEReader(stream);
     // Returns true if the DLL has a CLR header (metadata)
     return peReader.HasMetadata;
+}
+
+Machine GetDllArchitecture(string fileName)
+{
+    using var stream = File.OpenRead(fileName);
+    using var peReader = new PEReader(stream);
+    return peReader.PEHeaders.CoffHeader.Machine;
+    /*
+     * Machine.Amd64 indicates 64-bit.
+     * Machine.I386 indicates 32-bit (or potentially AnyCPU).
+     * Machine.ARM or Machine.ARM64 indicates ARM architecture. 
+     * To distinguish x86 from AnyCPU for .NET, you would inspect the CorHeader flags, similar to the CorFlags.exe tool. Native DLLs will correctly report their respective architecture, such as I386 (32-bit) or Amd64 (64-bit).
+     */
 }
