8c4e3f4e20f762352bf6d2f9f936ff8b8f239a12
javacommons <javacommons@gmail.com>
2026-04-03 09:42:13 +0900

    ⭕️ Commit at 2026.0403.0942.12 [https://github.com/nuget-pkg/EasyObject] [DIFF] diff --git a/+sub/cs-script-embed/cs-script-embed.main.cs b/+sub/cs-script-embed/cs-script-embed.main.cs     +using System;     -// 1. Define your source code     -string sourceCode = @"     +try     +{     +    // 1. Define your source code     +    string sourceCode = @"     -// 2. Identify the path to the NuGet DLL (Must be accessible at runtime)     -string jsonPath = @"C:\Users\Name\.nuget\packages\newtonsoft.json\13.0.1\lib\netstandard2.0\Newtonsoft.Json.dll";     +    // 2. Identify the path to the NuGet DLL (Must be accessible at runtime)     +    string jsonPath = @"C:\Users\user\.nuget\packages\newtonsoft.json\13.0.1\lib\netstandard2.0\Newtonsoft.Json.dll";     +     +    // 3. Create the compilation     +    var compilation = CSharpCompilation.Create("DynamicAssembly")     +        .WithOptions(new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary))     +        .AddReferences(     +            MetadataReference.CreateFromFile(typeof(object).Assembly.Location),     +            MetadataReference.CreateFromFile(jsonPath) // Adding the NuGet package reference     +        )     +        .AddSyntaxTrees(CSharpSyntaxTree.ParseText(sourceCode));     -// 3. Create the compilation     -var compilation = CSharpCompilation.Create("DynamicAssembly")     -    .WithOptions(new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary))     -    .AddReferences(     -        MetadataReference.CreateFromFile(typeof(object).Assembly.Location),     -        MetadataReference.CreateFromFile(jsonPath) // Adding the NuGet package reference     -    )     -    .AddSyntaxTrees(CSharpSyntaxTree.ParseText(sourceCode));     +    // 4. Emit to memory or file     +    using var ms = new MemoryStream();     +    var result = compilation.Emit(ms);     -// 4. Emit to memory or file     -using var ms = new MemoryStream();     -var result = compilation.Emit(ms);     +    if (result.Success)     +    {     +        ms.Seek(0, SeekOrigin.Begin);     +        Assembly assembly = Assembly.Load(ms.ToArray());     +        // Use reflection to run the code...     +    }     -if (result.Success)     +}     +catch (Exception ex)     -    ms.Seek(0, SeekOrigin.Begin);     -    Assembly assembly = Assembly.Load(ms.ToArray());     -    // Use reflection to run the code...     +    Console.WriteLine($"Error: {ex.Message}"); [/DIFF]

--- +sub/cs-script-embed/cs-script-embed.main.cs
+++ +sub/cs-script-embed/cs-script-embed.main.cs
@@ -1,36 +1,45 @@
 //css_nuget Microsoft.CodeAnalysis.CSharp
 //css_nuget Microsoft.CodeAnalysis.CSharp.Workspaces
+using System;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using System.IO;
 using System.Reflection;
 
-// 1. Define your source code
-string sourceCode = @"
+try
+{
+    // 1. Define your source code
+    string sourceCode = @"
     using Newtonsoft.Json;
     public class Evaluator {
         public string GetJson() => JsonConvert.SerializeObject(new { Greeting = ""Hello Roslyn"" });
     }";
 
-// 2. Identify the path to the NuGet DLL (Must be accessible at runtime)
-string jsonPath = @"C:\Users\Name\.nuget\packages\newtonsoft.json\13.0.1\lib\netstandard2.0\Newtonsoft.Json.dll";
+    // 2. Identify the path to the NuGet DLL (Must be accessible at runtime)
+    string jsonPath = @"C:\Users\user\.nuget\packages\newtonsoft.json\13.0.1\lib\netstandard2.0\Newtonsoft.Json.dll";
 
-// 3. Create the compilation
-var compilation = CSharpCompilation.Create("DynamicAssembly")
-    .WithOptions(new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary))
-    .AddReferences(
-        MetadataReference.CreateFromFile(typeof(object).Assembly.Location),
-        MetadataReference.CreateFromFile(jsonPath) // Adding the NuGet package reference
-    )
-    .AddSyntaxTrees(CSharpSyntaxTree.ParseText(sourceCode));
+    // 3. Create the compilation
+    var compilation = CSharpCompilation.Create("DynamicAssembly")
+        .WithOptions(new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary))
+        .AddReferences(
+            MetadataReference.CreateFromFile(typeof(object).Assembly.Location),
+            MetadataReference.CreateFromFile(jsonPath) // Adding the NuGet package reference
+        )
+        .AddSyntaxTrees(CSharpSyntaxTree.ParseText(sourceCode));
 
-// 4. Emit to memory or file
-using var ms = new MemoryStream();
-var result = compilation.Emit(ms);
+    // 4. Emit to memory or file
+    using var ms = new MemoryStream();
+    var result = compilation.Emit(ms);
 
-if (result.Success)
+    if (result.Success)
+    {
+        ms.Seek(0, SeekOrigin.Begin);
+        Assembly assembly = Assembly.Load(ms.ToArray());
+        // Use reflection to run the code...
+    }
+
+}
+catch (Exception ex)
 {
-    ms.Seek(0, SeekOrigin.Begin);
-    Assembly assembly = Assembly.Load(ms.ToArray());
-    // Use reflection to run the code...
+    Console.WriteLine($"Error: {ex.Message}");
 }
