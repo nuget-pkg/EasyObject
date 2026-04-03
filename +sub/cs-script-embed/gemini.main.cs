789334710e832c1ee1d9d4f7b7f3720a500b8159
javacommons <javacommons@gmail.com>
2026-04-03 17:09:11 +0900

    ⭕️ Commit at 2026.0403.1709.10 [https://github.com/nuget-pkg/EasyObject] [DIFF] diff --git a/+sub/cs-script-embed/gemini.main.cs b/+sub/cs-script-embed/gemini.main.cs     -//xxxcss_nuget CS-Script.Evaluator     -     -     -//CSScript.Evaluator.With(     -//    static eval => {     -//        eval.IsCachingEnabled = false;     -//        eval.ReferenceAssembliesFromCode = true; // コード内の //css_reference を有効にする     -//    }     -//    );     -     -// 1. NuGetを自動解決するための設定     -// これにより //css_nuget が有効になります     -//CSScript.EvaluatorConfig.ReferencingAlgorithm = ReferencingAlgorithm.MetaData;     -     -     -     -    //CSScript.Evaluator.With(     -    //    static eval =>     -    //    {     -    //        eval.IsCachingEnabled = false;     -    //        eval.CompileMethod = CompileMethod.CompileToMemory; // メモリ上でコンパイル     -    //    }     -    //    );     -        //eval.CompileMethod = CompileMethod.CompileToMemory; // メモリ上でコンパイル     -     -    //var classes = assembly!.GetTypes()     -    //                      .Where(t => t.IsClass);     -    //foreach (var type in classes)     -    //{     -    //    Console.WriteLine($"Found all of classes: {type.FullName}");     -    //}     -    //Console.WriteLine($"Error: {ex.Message}"); [/DIFF]

--- +sub/cs-script-embed/gemini.main.cs
+++ +sub/cs-script-embed/gemini.main.cs
@@ -1,24 +1,10 @@
 //css_nuget CS-Script
 //css_nuget EasyObject
-//xxxcss_nuget CS-Script.Evaluator
 using CSScripting;
 using CSScriptLib;
 using System;
 using System.Linq;
 using static Global.EasyObject;
-
-
-//CSScript.Evaluator.With(
-//    static eval => {
-//        eval.IsCachingEnabled = false;
-//        eval.ReferenceAssembliesFromCode = true; // コード内の //css_reference を有効にする
-//    }
-//    );
-
-// 1. NuGetを自動解決するための設定
-// これにより //css_nuget が有効になります
-//CSScript.EvaluatorConfig.ReferencingAlgorithm = ReferencingAlgorithm.MetaData;
-
 try
 {
     // 実行したいコード (//css_nuget を含める)
@@ -26,7 +12,6 @@ try
         //css_nuget Newtonsoft.Json
         using System;
         using Newtonsoft.Json;
-
         public class Script
         {
             public void Run()
@@ -35,32 +20,16 @@ try
                 Console.WriteLine(JsonConvert.SerializeObject(data, Formatting.Indented));
             }
         }";
-
-    //CSScript.Evaluator.With(
-    //    static eval =>
-    //    {
-    //        eval.IsCachingEnabled = false;
-    //        eval.CompileMethod = CompileMethod.CompileToMemory; // メモリ上でコンパイル
-    //    }
-    //    );
     var script = CSScript.Evaluator
         .ReferenceAssemblyByName("System.Runtime") // これがないと Newtonsoft.Json が見つからないことがあります
                      .ReferenceAssembliesFromCode(code);
     CSScript.Evaluator.With(static eval =>
     {
         eval.IsCachingEnabled = false;
-        //eval.CompileMethod = CompileMethod.CompileToMemory; // メモリ上でコンパイル
-
     });
     var assembly = script.CompileMethod(code);
     Log(assembly != null);
     ExpectTrue(assembly != null);
-    //var classes = assembly!.GetTypes()
-    //                      .Where(t => t.IsClass);
-    //foreach (var type in classes)
-    //{
-    //    Console.WriteLine($"Found all of classes: {type.FullName}");
-    //}
     var classes = assembly!.GetExportedTypes()
                           .Where(t => t.IsClass);
     foreach (var type in classes)
@@ -76,5 +45,4 @@ try
 catch (Exception ex)
 {
     Abort(ex);
-    //Console.WriteLine($"Error: {ex.Message}");
 }
