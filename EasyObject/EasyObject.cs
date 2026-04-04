c3dcf9e721a683d8af3e7ab98fc9b49e40f3c065
javacommons <javacommons@gmail.com>
2026-04-05 02:52:22 +0900

    ⭕️ Commit at 2026.0405.0252.21 [https://github.com/nuget-pkg/EasyObject] [DIFF] diff --git a/EasyObject/EasyObject.cs b/EasyObject/EasyObject.cs     -internal class MiniEasyObjectConverter : IConvertParsedResult     -#else     -internal class EasyObjectConverter : IConvertParsedResult     -#endif     -{     -    public object? ConvertParsedResult(object? x, string origTypeName) {     -        if (x is Dictionary<string, object> dict) {     -            var keys = dict.Keys;     -            var result = new Dictionary<string, EasyObject>();     -            foreach (var key in keys) {     -                var eo = new EasyObject {     -                    RealData = dict[key]     -                };     -                result[key] = eo;     -            }     -            return result;     -        }     -        if (x is List<object> list) {     -            var result = new List<EasyObject>();     -            foreach (var e in list) {     -                var eo = new EasyObject {     -                    RealData = e     -                };     -                result.Add(eo);     -            }     -            return result;     -        }     -        return x;     -    }     -}     -#if MINIMAL diff --git a/EasyObject/EasyObjectDetails.cs b/EasyObject/EasyObjectDetails.cs     -﻿namespace Global;     -public class EasyObjectDetails {     -     +﻿#if MINIMAL     +using EasyObject = Global.MiniEasyObject;     +#endif     +     +using System.Collections.Generic;     +namespace Global;     +#if MINIMAL     +internal class MiniEasyObjectConverter : IConvertParsedResult     +#else     +internal class EasyObjectConverter : IConvertParsedResult     +#endif     +{     +    public object? ConvertParsedResult(object? x, string origTypeName) {     +        if (x is Dictionary<string, object> dict) {     +            var keys = dict.Keys;     +            var result = new Dictionary<string, EasyObject>();     +            foreach (var key in keys) {     +                var eo = new EasyObject {     +                    RealData = dict[key]     +                };     +                result[key] = eo;     +            }     +            return result;     +        }     +        if (x is List<object> list) {     +            var result = new List<EasyObject>();     +            foreach (var e in list) {     +                var eo = new EasyObject {     +                    RealData = e     +                };     +                result.Add(eo);     +            }     +            return result;     +        }     +        return x;     +    } diff --git a/MiniEasyObject/MiniEasyObject.csproj b/MiniEasyObject/MiniEasyObject.csproj     -		<!-- https://gemini.google.com/share/62f63a243809 -->     -		<AllowedOutputExtensionsInPackageBuildOutputFolder>$(AllowedOutputExtensionsInPackageBuildOutputFolder);.pdb</AllowedOutputExtensionsInPackageBuildOutputFolder>     -		<DebugType>portable</DebugType>     -		<PublishRepositoryUrl>true</PublishRepositoryUrl>     -		<EmbedUntrackedSources>true</EmbedUntrackedSources>     -	</PropertyGroup>     +        <!-- https://gemini.google.com/share/62f63a243809 -->     +        <AllowedOutputExtensionsInPackageBuildOutputFolder>$(AllowedOutputExtensionsInPackageBuildOutputFolder);.pdb</AllowedOutputExtensionsInPackageBuildOutputFolder>     +        <DebugType>portable</DebugType>     +        <PublishRepositoryUrl>true</PublishRepositoryUrl>     +        <EmbedUntrackedSources>true</EmbedUntrackedSources>     +    </PropertyGroup>     -	<PropertyGroup>     +    <PropertyGroup>     +        <Compile Include="..\EasyObject\EasyObjectDetails.cs">     +            <Link>EasyObjectDetails.cs</Link>     +        </Compile> [/DIFF]

--- EasyObject/EasyObject.cs
+++ EasyObject/EasyObject.cs
@@ -43,37 +43,6 @@ public enum EasyObjectType
     @null
 }
 #if MINIMAL
-internal class MiniEasyObjectConverter : IConvertParsedResult
-#else
-internal class EasyObjectConverter : IConvertParsedResult
-#endif
-{
-    public object? ConvertParsedResult(object? x, string origTypeName) {
-        if (x is Dictionary<string, object> dict) {
-            var keys = dict.Keys;
-            var result = new Dictionary<string, EasyObject>();
-            foreach (var key in keys) {
-                var eo = new EasyObject {
-                    RealData = dict[key]
-                };
-                result[key] = eo;
-            }
-            return result;
-        }
-        if (x is List<object> list) {
-            var result = new List<EasyObject>();
-            foreach (var e in list) {
-                var eo = new EasyObject {
-                    RealData = e
-                };
-                result.Add(eo);
-            }
-            return result;
-        }
-        return x;
-    }
-}
-#if MINIMAL
 public class MiniEasyObject :
 #else
 public class
--- EasyObject/EasyObjectDetails.cs
+++ EasyObject/EasyObjectDetails.cs
@@ -1,4 +1,37 @@
-﻿namespace Global;
-public class EasyObjectDetails {
-    
+﻿#if MINIMAL
+using EasyObject = Global.MiniEasyObject;
+#endif
+
+using System.Collections.Generic;
+namespace Global;
+#if MINIMAL
+internal class MiniEasyObjectConverter : IConvertParsedResult
+#else
+internal class EasyObjectConverter : IConvertParsedResult
+#endif
+{
+    public object? ConvertParsedResult(object? x, string origTypeName) {
+        if (x is Dictionary<string, object> dict) {
+            var keys = dict.Keys;
+            var result = new Dictionary<string, EasyObject>();
+            foreach (var key in keys) {
+                var eo = new EasyObject {
+                    RealData = dict[key]
+                };
+                result[key] = eo;
+            }
+            return result;
+        }
+        if (x is List<object> list) {
+            var result = new List<EasyObject>();
+            foreach (var e in list) {
+                var eo = new EasyObject {
+                    RealData = e
+                };
+                result.Add(eo);
+            }
+            return result;
+        }
+        return x;
+    }
 }
